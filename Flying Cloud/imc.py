772889aaf10e03fb22c559876d2b8f8868bec739
jhowji <74212057+jhowji@users.noreply.github.com>
2020-11-09 17:50:43 -0300

    Add files via upload
    A reinterpreted version of flappy bird

---
+++ Flying Cloud/imc.py
@@ -0,0 +1,9 @@
+algoritimo Imc
+inicio
+    real: altura, peso, imc;
+    escreva ("digite sua altura");
+    leia (altura);
+    escreva("digite seu peso");
+    leia(peso);
+    imc = peso/(alutra*altura)
+    escreva: ("o imc da pessoa é: ")
